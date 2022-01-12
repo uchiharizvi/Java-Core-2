4bd89ded3c286d390b97022baf9e32025037063d
Kavish Rizvi <rizvikavish205@gmail.com>
2022-01-13 01:43:37 +0530

    sorting algos

---
+++ src/datastructures/trees/binarytree/problems/topview/TopViewTree.java
@@ -0,0 +1,19 @@
+package datastructures.trees.binarytree.problems.topview;
+
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class TopViewTree {
+    Node root;
+
+    public TopViewTree() {
+        root = null;
+    }
+    private void topView(Node root){
+        Queue<TopViewQueue> queue = new LinkedList<>();
+        /*Map<Integer, Node> topViewMap =n*/
+    }
+}
---
+++ src/sorting/bubble/Demo.java
@@ -0,0 +1,13 @@
+package sorting.bubble;
+
+import sorting.PrintArray;
+
+public class Demo {
+    public static void main(String[] args) {
+        BubbleSort bs = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        bs.sort(arr);
+        System.out.println("Bubble Sorted :");
+        PrintArray.print(arr);
+    }
+}
