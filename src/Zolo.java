5ad85ac979e00cf3826192dec2cdc2afba8dfe08
Kavish Rizvi <rizvikavish205@gmail.com>
2022-02-16 17:31:04 +0530

    zolo stays

---
+++ src/Zolo.java
@@ -0,0 +1,12 @@
+public class Zolo {
+    public static void main(String[] args) {
+        //emp-empcode,user id and user -user id Primary and profile - user id,email
+        //sel a.email
+        // from profile as a
+        //right join profile as b
+        //on a.userid = b.user id
+        //where a.empcode in ('123')
+
+
+    }
+}
